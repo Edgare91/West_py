c79f99ef4a4fe6e450643efdc5323c3089928ab5
Edgar Nunez <nure.edgare@gmail.com>
2023-02-27 23:29:01 -0800

    Import Gift Code
    All Modules and JSON to src

---
+++ src/gladysUserLogin.py
@@ -0,0 +1,15 @@
+import io
+
+"""
+	Student: Edgar Nunez
+	Module: gladysUserLogin
+	Description: This module allows or denied the access to the app depending if the user enter
+	a correct email adress
+"""
+
+# Login, email and a password requiered to proced to the features app
+
+
+def login():
+    email = input("Please enter your email adress: ")
+    password = input("Please enter your password: ")
